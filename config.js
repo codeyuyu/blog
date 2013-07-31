e34da74b6453cff91567656b28b8686b665ceca2
codeyuyu <codeyuyu@gmail.com>
2013-07-31 16:19:00 +0800

    修改
    ~

--- config.js
+++ config.js
@@ -37,4 +37,4 @@ exports.config = {
 };
 
 //mongodb settings
-exports.db = mongoskin.db(process.env.MONGOLAB_URI || "mongodb://localhost:27017/test");//数据库连接串
+exports.db = mongoskin.db(process.env.MONGOLAB_URI || "mongodb:~~~~~~");//数据库连接串
